18adfc0044d358861cf0301f6ae26814f16c1229
Abhish Khanal <abheeshkhanal@gmail.com>
2020-08-25 16:39:18 +0545

    Connection status and speed monitoring add

--- Digimesh Telemetry/xbee_to_udp.py
+++ Digimesh Telemetry/xbee_to_udp.py
@@ -33,8 +33,6 @@ logger=logging.getLogger()
 '''
 logger.setLevel(logging.INFO)
 
-SCRIPT_START = time.time()
-
 
 class HandleUDP():
     def __init__(self, address, gcs, udp_ip, udp_port):
@@ -44,6 +42,14 @@ class HandleUDP():
         self.drone_id = address
         # To check if error occured during initialization
         self.__initialise_error = False
+        self.xbee_bytes_received = 0
+        self.udp_bytes_sent = 0
+
+        self._time_last_data_rx = time.time()
+        self._connection_ok = True
+        self._speed = 0
+
+        
 
         #create object for remote xbee device
         try:
@@ -63,6 +69,9 @@ class HandleUDP():
 
         udp_rx = threading.Thread(target= self.udp_rx_thread, daemon = True)
         udp_rx.start()
+
+        monitor_data = threading.Thread(target=self.monitor_connection,daemon = True)
+        monitor_data.start()
     
     def udp_rx_thread(self):
         while True:
@@ -79,9 +88,20 @@ class HandleUDP():
     def send_to_udp(self,indata):
         try:
             sent = self.socket.send(indata)
+            #self.bytes_sent += sent 
         except:
             pass
+    
+    def monitor_connection(self):
+        while True:
+            connection_lost_time = time.time() - self._time_last_data_rx
+            if connection_lost_time > 3:
+                self._connection_ok = False
+            else:
+                self._connection_ok = True
+            time.sleep(0.5)
 
+    
 
 
 class GCS():
@@ -94,6 +114,8 @@ class GCS():
         #UDP variables
         self.udp_ip, self.udp_port = udp
         self.udp_connections = {}
+        self.connected_drone = []
+
     
     def __del__(self):
         self.gcs.close()
@@ -104,12 +126,17 @@ class GCS():
         # handle_data deals with the incoming of XBee data
         self.gcs.add_data_received_callback(self.handle_data)
         time.sleep(0.1)
+        monitor_drone = threading.Thread(target=self.monitor_drones,daemon = True)
+        monitor_drone.start()
+
         
     def handle_data(self,xbee_message):
+
         indata = bytes(xbee_message.data)
         address = xbee_message.remote_device.get_64bit_addr()
 
         if address not in self.udp_connections:
+            self.connected_drone.append(address)
             self.udp_connections[address] = HandleUDP(str(address),self.gcs,self.udp_ip,self.udp_port)
             if self.udp_connections[address].initialise_error is False:
                 info = {'context':'UDP_CONN','msg':'Initialized UDP port at 127.0.0.1:'+str(self.udp_port)+' for drone ID:'+str(address)}
@@ -121,7 +148,24 @@ class GCS():
                 info = {'context':'UDP_CONN','msg':'Failed to initialize UDP port for drone ID:'+str(address)+',Restarting UDP Connection'}
                 logger.info(info)
 
+        data_rx_time = time.time()
+        dt = data_rx_time - self.udp_connections[address]._time_last_data_rx
+        self.udp_connections[address]._time_last_data_rx = data_rx_time
+
+        self.udp_connections[address]._speed = len(indata)/dt
+    
         self.udp_connections[address].send_to_udp(indata)
+        
+    def monitor_drones(self):
+        while True:
+            for address in self.connected_drone:
+                # if last time of data received is greater than 3 seconds then close udp connection for that address.
+                #if not self.udp_connections[address]._connection_ok:
+                    #del self.udp_connections[address]
+                
+                print("Speed = ",self.udp_connections[address]._speed/1000," OK:",self.udp_connections[address]._connection_ok)
+
+            time.sleep(0.5)
 
 
 if __name__ == '__main__':
